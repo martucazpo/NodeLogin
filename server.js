123d3919826d7f2da5da42d27b9639b46d816b6c
martucazpo <martucazpo@gmail.com>
2019-11-25 20:30:51 -0700

    restyled login

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ initializePassport(
 const users = []
 
 app.set('view-engine', 'ejs')
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended:false}))
 app.use(flash())
 app.use(session({
